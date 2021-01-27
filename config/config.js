49f8a078cf2d04d85f6df8f511571de91a569548
staefloded <michaeleleyowo29@gmail.com>
2021-01-27 13:57:01 +0000

    .

--- config/config.js
+++ config/config.js
@@ -15,20 +15,20 @@ module.exports = {
     host: "traineedb.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
     dialect: 'postgres' 
   },
-  // production: {
-  //   username: "CHANGE_ME",
-  //   password: "CHANGE_ME",
-  //   database: "grader",
-  //   host: "manifestdbinstance.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
-  //   dialect: 'postgres' 
-  // }
-  // ,
-  // production: {
-  //   username: process.env.DB_USERNAME,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_NAME,
-  //   host: process.env.DB_HOSTNAME,
-  //   dialect: 'mysql',
-  //   use_env_variable: 'DATABASE_URL'
-  // }
+  production: {
+    username: "traineeUser",
+    password: "traineePassword",
+    database: "grader",
+    host: "manifestdbinstance.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
+    dialect: 'postgres' 
+  }
+  ,
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+    use_env_variable: 'DATABASE_URL'
+  }
 };
